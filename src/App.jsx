c9059283ef2eddc47de6f0fb3cbb176177333dcf
fabiogerritferrari <fabiogerritferrari@gmail.com>
2026-03-09 17:34:27 +0100

    Creati componenti che mi servono per la mia pagina e collegati su app tramite router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HomePage from './components/HomePage'
+import AboutPage from './components/AboutPage'
+import ProductsPage from './components/ProductsPage'
+import DefaultLayout from './components/layout/DefaultLayout'
 
 function App() {
 
 
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<DefaultLayout />}>
+          <Route path='/' element={<HomePage />}></Route>
+          <Route path='/About' element={<AboutPage />}></Route>
+          <Route path='/Products' element={<ProductsPage />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
