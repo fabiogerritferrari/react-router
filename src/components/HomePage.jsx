801b78b0add79d85b1589b098b1bf29a4d9634dd
fabiogerritferrari <fabiogerritferrari@gmail.com>
2026-03-09 18:13:12 +0100

    creato jumbotron inserito nella home page feat: cambiata visuallizzazione tramite css e bootstrap

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,11 @@
+export default function HomePage() {
+
+    return (
+        <div className="container">
+            <div className="jumbotron bg-success text-success-emphasis mt-5 d-flex justify-content-center align-items-center">
+                <h1 className="text-center fs-1 fw-bold">Benvenuti nel mio store</h1>
+            </div>
+        </div>
+    )
+
+}
---
+++ src/components/layout/DefaultLayout.jsx
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router-dom";
+import MainNav from "./MainNav";
+
+
+export default function DefaultLayout() {
+
+    return (
+        <div>
+            <header>
+                <MainNav />
+            </header>
+            <main>
+                <Outlet />
+            </main>
+        </div>
+    )
+
+}
